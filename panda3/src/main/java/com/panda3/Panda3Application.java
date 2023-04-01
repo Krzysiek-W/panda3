1a317238af743989cd14b50d4b15b931c6d260fa
Krzysztof <k.wiesebach@gmail.com>
2023-04-01 20:19:02 +0200

    init commit

---
+++ panda3/src/main/java/com/panda3/Panda3Application.java
@@ -0,0 +1,13 @@
+package com.panda3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Panda3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Panda3Application.class, args);
+    }
+
+}
---
+++ panda3/src/test/java/com/panda3/Panda3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Panda3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
